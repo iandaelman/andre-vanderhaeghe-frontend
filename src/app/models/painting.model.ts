5dfab972779648afff90c7e7e73206b1c737a0b8
Ian Daelman <iandaelman@hotmail.com>
2025-10-15 15:29:20 +0200

    added painting model

--- src/app/models/painting.model.ts
+++ src/app/models/painting.model.ts
@@ -1,1 +1,9 @@
-export interface Painting {}
+export interface PaintingModel {
+  id: number;
+  title: string;
+  category: string;
+  length: number;
+  width: number;
+  imageUrl: string;
+  description: string;
+}
