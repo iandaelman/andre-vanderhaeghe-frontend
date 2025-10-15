2ce84d18bc73947ea59669c3d9666d8fc1799088
Ian Daelman <iandaelman@hotmail.com>
2025-10-15 15:49:00 +0200

    added paintingService

--- src/app/services/paintings.service.ts
+++ src/app/services/paintings.service.ts
@@ -1,8 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { PaintingModel } from '../models/painting.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PaintingsService {
-  
+  private dataUrl = 'assets/data/paintings.json';
+
+  constructor(private http: HttpClient) {}
+
+  getPaintings(): Observable<PaintingModel[]> {
+    return this.http.get<PaintingModel[]>(this.dataUrl);
+  }
+
+  getPaintingById(id: number): Observable<PaintingModel | undefined> {
+    return this.getPaintings().pipe(map((paintings) => paintings.find((p) => p.id === id)));
+  }
 }
