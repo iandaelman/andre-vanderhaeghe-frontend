90667ebcb2cab4f257f5cf43878667426e1dc649
Ian Daelman <iandaelman@hotmail.com>
2025-10-15 15:21:41 +0200

    added painting component and model

---
+++ src/app/components/painting/painting.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-painting',
+  imports: [],
+  templateUrl: './painting.component.html',
+  styleUrl: './painting.component.css',
+})
+export class PaintingComponent {}
