608c76e2158740fb3aeacb7f4ef9126bead584ff
Ian Daelman <iandaelman@hotmail.com>
2025-12-07 15:41:53 +0100

    remove duplicate component

--- src/app/modules/paintings/pages/painting-details-page/painting-details-page.ts
+++ src/app/modules/paintings/pages/painting-details-page/painting-details-page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { PaintingModel } from '../../models/painting.model';
+import { ActivatedRoute } from '@angular/router';
+import { PaintingsService } from '../../services/paintings.service';
 
 @Component({
   selector: 'app-painting-details-page',
@@ -7,5 +10,11 @@ import { Component } from '@angular/core';
   styleUrl: './painting-details-page.css',
 })
 export class PaintingDetailsPage {
+  painting!: PaintingModel;
 
+  constructor(private route: ActivatedRoute, private paintingService: PaintingsService) {}
+  ngOnInit(): void {
+    const id = +this.route.snapshot.paramMap.get('id')!;
+    this.painting = this.paintingService.getPaintingById(id);
+  }
 }
