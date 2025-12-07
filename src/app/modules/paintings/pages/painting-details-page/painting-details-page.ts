c78ea47c0d40a5ad2a329b965085a943da2c2cd8
Ian Daelman <iandaelman@hotmail.com>
2025-12-07 16:44:43 +0100

    painting details done

--- src/app/modules/paintings/pages/painting-details-page/painting-details-page.ts
+++ src/app/modules/paintings/pages/painting-details-page/painting-details-page.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { PaintingModel } from '../../models/painting.model';
 import { ActivatedRoute } from '@angular/router';
 import { PaintingsService } from '../../services/paintings.service';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-painting-details-page',
-  imports: [],
+  imports: [MatExpansionModule],
   templateUrl: './painting-details-page.html',
   styleUrl: './painting-details-page.css',
 })
