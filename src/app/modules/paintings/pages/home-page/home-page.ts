4bff2b7c96cebf90111f7c1c79f34441abc68e29
Ian Daelman <iandaelman@hotmail.com>
2026-06-28 14:04:56 +0200

    home pagina toegevoegd

--- src/app/modules/paintings/pages/home-page/home-page.ts
+++ src/app/modules/paintings/pages/home-page/home-page.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
-export class HomePage {}
+export class HomePage {
+  protected navigationLinks = [
+    { label: 'Gallerij', route: '/gallerij' },
+    { label: 'Biografie', route: '/biografie' },
+    { label: 'Contact', route: '/contact' },
+  ];
+}
